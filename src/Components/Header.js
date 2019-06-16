19c06d42baa04e3e164f2b3a6ef431c3dabb93bc
Andrii Korzh <korzh.andrii@gmail.com>
2019-06-15 20:03:02 -0700

    fix navigation certs

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -20,6 +20,7 @@ class Header extends Component {
 				<nav id="nav-wrap">
 					<a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
 					<a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+					<Certificates/>
 					<ul id="nav" className="nav">
 						<li className="current"><a className="smoothscroll" href="#home">Home</a></li>
 						<li><a className="smoothscroll" href="#about">About</a></li>
@@ -28,7 +29,6 @@ class Header extends Component {
 						{/*<li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>*/}
 						{/*<li><a className="smoothscroll" href="#contact">Contact</a></li>*/}
 					</ul>
-					<Certificates/>
 				</nav>
 				<div className="row banner">
 					<div className="banner-text">
